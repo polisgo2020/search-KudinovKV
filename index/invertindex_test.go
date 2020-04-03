f25f23955fc9c81830dc50762c9210b9e608222f
Кирилл Кудинов <=>
2020-04-03 11:48:34 +0300

    hm4. v2.0

--- index/invertindex_test.go
+++ index/invertindex_test.go
@@ -1,7 +1,7 @@
 package index
 
 import (
-	"strings"
+	"reflect"
 	"sync"
 	"testing"
 )
@@ -32,47 +32,19 @@ func TestContains(t *testing.T) {
 }
 
 func TestParseIndexFile(t *testing.T) {
-	in := string("a:2\nbanana:2\nis:0,1,2\nit:0,1,2\nwhat:0,1\n")
+	in := string("is:0,1,2\na:2\nbanana:2\nit:0,1,2\nwhat:0,1\n")
 	expected := []int{0, 1, 2}
 	actual := i.ParseIndexFile(in)
-	if !testEq(actual, expected) {
+	if !reflect.DeepEqual(actual, expected) {
 		t.Errorf("%v is not equal to expected %v", actual, expected)
 	}
 	listOfFiles = actual
 }
-
-func testEq(actual, expected []int) bool {
-	// If one is nil, the other must also be nil.
-	if (actual == nil) != (expected == nil) {
-		return false
-	}
-
-	if len(actual) != len(expected) {
-		return false
-	}
-
-	flag := false
-	for j := range actual {
-		flag = false
-		for k := range expected {
-			if actual[j] == expected[k] {
-				flag = true
-				break
-			}
-		}
-		if flag == false {
-			return false
-		}
-	}
-
-	return true
-}
-
 func TestMakeSearch(t *testing.T) {
 	in := []string{"banana", "is"}
-	expected := []int{1, 1, 2}
+	expected := []int{2, 1, 1}
 	actual := i.MakeSearch(in, listOfFiles)
-	if !testEq(actual, expected) {
+	if !reflect.DeepEqual(actual, expected) {
 		t.Errorf("%v is not equal to expected %v", actual, expected)
 	}
 }
@@ -81,33 +53,16 @@ func TestPrepareTokens(t *testing.T) {
 	in := string("Hello 123 is world !")
 	expected := []string{"hello", "world"}
 	actual := PrepareTokens(in)
-	if !testStringEq(actual, expected) {
+	if !reflect.DeepEqual(actual, expected) {
 		t.Errorf("%v is not equal to expected %v", actual, expected)
 	}
-}
-
-func testStringEq(actual, expected []string) bool {
-	// If one is nil, the other must also be nil.
-	if (actual == nil) != (expected == nil) {
-		return false
-	}
-	if len(actual) != len(expected) {
-		return false
-	}
-
-	for i := range actual {
-		if ok := strings.EqualFold(actual[i], expected[i]); !ok {
-			return false
-		}
-	}
-	return true
 }
 
 func TestAddToken(t *testing.T) {
 	expected := NewInvertIndex()
 	expected["newtoken"] = append(expected["newtoken"], 1)
 	in.addToken("newtoken", 1)
-	if !testEq(in["newtoken"], expected["newtoken"]) {
+	if !reflect.DeepEqual(in["newtoken"], expected["newtoken"]) {
 		t.Errorf("%v is not equal to expected %v", in, expected)
 	}
 }
@@ -124,7 +79,7 @@ func TestListener(t *testing.T) {
 	dataCh <- []string{"newtoken", "2"}
 	close(dataCh)
 	mutex.Lock()
-	if !testEq(in["newtoken"], expected["newtoken"]) {
+	if !reflect.DeepEqual(in["newtoken"], expected["newtoken"]) {
 		t.Errorf("%v is not equal to expected %v", in, expected)
 	}
 	mutex.Unlock()
