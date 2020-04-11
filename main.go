fa4b3d24b2799fc4a7132b7e1b7af0eedc052a74
Кирилл Кудинов <=>
2020-04-12 00:17:11 +0300

    hm5. v2.0

--- main.go
+++ main.go
@@ -1,106 +1,26 @@
 package main
 
 import (
-	"html/template"
 	"io/ioutil"
-	"net/http"
 	"os"
 	"path/filepath"
-	"time"
 
 	"github.com/polisgo2020/search-KudinovKV/config"
 	"github.com/polisgo2020/search-KudinovKV/file"
 	"github.com/polisgo2020/search-KudinovKV/index"
+	"github.com/polisgo2020/search-KudinovKV/web"
 	"github.com/rs/zerolog"
 	zl "github.com/rs/zerolog/log"
 )
 
-var (
-	listOfFiles []string
-	i           *index.InvertIndex
-	searchTmpl  *template.Template
-	indexTmpl   *template.Template
-)
-
-func searchPage(w http.ResponseWriter, r *http.Request) {
-	tokens := r.FormValue("tokens")
-	if tokens == "" {
-		zl.Debug().
-			Msg("Incorrect request, cant find tokens field")
-		http.Redirect(w, r, "/", http.StatusFound)
-		return
-	}
-	out := i.MakeSearch(index.PrepareTokens(tokens), listOfFiles)
-
-	err := searchTmpl.ExecuteTemplate(w, "search.html",
-		struct {
-			Result []index.Rate
-		}{
-			out,
-		})
-
-	if err != nil {
-		zl.Fatal().Err(err).
-			Msg("Can't execute search template")
-	}
-}
-
-func logMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		next.ServeHTTP(w, r)
-		zl.Debug().
-			Str("method", r.Method).
-			Str("remote", r.RemoteAddr).
-			Str("path", r.URL.Path).
-			Int("duration", int(time.Since(start))).
-			Msgf("Called url %s", r.URL.Path)
-	})
-}
-
 func searchMain(cfg config.Config, filename string) {
-
 	data, err := file.ReadFile(filename)
 	if err != nil {
 		zl.Fatal().Err(err).
 			Msg("Can't read index file ")
 	}
-	searchTmpl, err = template.ParseFiles("template/search.html")
-	if err != nil {
-		zl.Fatal().Err(err).
-			Msg("Can't parse search template")
-	}
 
-	indexTmpl, err = template.ParseFiles("template/index.html")
-	if err != nil {
-		zl.Fatal().Err(err).
-			Msg("Can't parse index template")
-	}
-
-	i = index.NewInvertIndex()
-	listOfFiles = i.ParseIndexFile(data)
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("/search", searchPage)
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		err = indexTmpl.ExecuteTemplate(w, "index.html", struct{}{})
-		if err != nil {
-			zl.Fatal().Err(err).
-				Msg("Can't execute index template")
-		}
-	})
-
-	siteHandler := logMiddleware(mux)
-	server := http.Server{
-		Addr:         cfg.Listen,
-		Handler:      siteHandler,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 10 * time.Second,
-	}
-
-	zl.Debug().
-		Msg("[ + ] starting server at " + cfg.Listen)
-	server.ListenAndServe()
+	web.StartServer(cfg, data)
 }
 
 func buildMain(dirName, resultFilename string) {
